c7141d0da2e7f0fba3bdfc3b5587f1380fea4883
AgungFathul <lowerbracket129@gmail.com>
2024-06-10 19:52:15 +0700

    first commit

--- database/migrations/2024_06_07_141132_create_registration_settings_table.php
+++ database/migrations/2024_06_07_141132_create_registration_settings_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('registration_settings', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('tournament_id');
             $table->enum('jenis', ['Individu', 'Tim']);
             $table->integer('jumlah_peserta')->nullable();
             $table->integer('jumlah_anggota_tim')->nullable();
